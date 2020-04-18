73b0db5ad6c4d54b916a16eb9b6a2adbe7e266ab
Tobias Käs <tobias.kaes@gmail.com>
2020-04-18 13:49:15 +0200

    cleanup

--- Match3/MainForm.Designer.cs
+++ Match3/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Match3
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -30,13 +30,13 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(524, 489);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "Match 3";
             this.ResumeLayout(false);
 
         }
--- Match3/MainForm.cs
+++ Match3/MainForm.cs
@@ -1,22 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Match3
 {
-    static class Program
+    public partial class MainForm : Form
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
         }
     }
 }
