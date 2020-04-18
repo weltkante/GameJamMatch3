d1b7f364cdeb339ad651512bb5801ca7e60a2729
Tobias Käs <tobias.kaes@gmail.com>
2020-04-18 13:58:51 +0200

    setup device

--- Match3/MainForm.cs
+++ Match3/MainForm.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SharpDX.Direct3D11;
 
 namespace Match3
 {
@@ -28,5 +30,43 @@ namespace Match3
 
     public class DisplayControl : Control
     {
+        private SharpDX.DXGI.Factory5 mGraphicsFactory;
+        private SharpDX.DXGI.SwapChain4 mGraphicsDisplay;
+
+        private Device5 mRenderDevice;
+        private DeviceContext4 mRenderContext;
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            if (DesignMode) return;
+
+            using (var factory = new SharpDX.DXGI.Factory2(Debugger.IsAttached))
+                mGraphicsFactory = factory.QueryInterface<SharpDX.DXGI.Factory5>();
+
+            using (var device = new Device(SharpDX.Direct3D.DriverType.Hardware, DeviceCreationFlags.BgraSupport | (Debugger.IsAttached ? DeviceCreationFlags.Debug | DeviceCreationFlags.Debuggable : DeviceCreationFlags.None)))
+                mRenderDevice = device.QueryInterface<Device5>();
+
+            mRenderContext = mRenderDevice.ImmediateContext.QueryInterface<DeviceContext4>();
+        }
+
+        protected override void OnHandleDestroyed(EventArgs e)
+        {
+            Utils.Dispose(ref mRenderContext);
+            Utils.Dispose(ref mGraphicsDisplay);
+            Utils.Dispose(ref mRenderDevice);
+            Utils.Dispose(ref mGraphicsFactory);
+
+            base.OnHandleDestroyed(e);
+        }
+    }
+
+    public static class Utils
+    {
+        public static void Dispose<T>(ref T field) where T : IDisposable
+        {
+            field?.Dispose();
+            field = default;
+        }
     }
 }
